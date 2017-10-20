3d7dde676ea12084cfa3d06c5ae200624915caf3
ku22332 <kunalaggarwal@dal.ca>
2017-10-20 11:56:10 -0700

    used for loop

--- 3rd.py
+++ 3rd.py
@@ -8,4 +8,13 @@ new_ip="192."
 def main():
    f1=open("running-config.cfg")
    f2 = open(conf-txt,"w")
-  
+    for line in f1:
+        line_change = line.edit(old_ip,new_ip)
+        f2.write(line_change)
+    f1.close()
+    f2.close()
+
+if __name__ == '__main__':
+    main()
+	
+
