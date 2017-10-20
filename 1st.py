d4c19b4b5118341997a8d466876a413c1e698986
ku22332 <kunalaggarwal@dal.ca>
2017-10-20 11:41:14 -0700

    opened files to be read

--- 1st.py
+++ 1st.py
@@ -1,17 +1,3 @@
 f = ["Book1.txt", "Book2.txt", "Book3.txt"]
 
-def main():
-    big_word = ""
-    for books in f:
-        fin = open(books)
-        for line in fin:
-            line_stripped = line.strip()
-            line_words = line_stripped.split()
-            for word in line_words:
-                if len(word) > len(big_word):
-                    big_word = word
-        fin.close()
-    print("Longest word : ", big_word)
 
-if __name__ == '__main__':
-    main()
