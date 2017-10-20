bf7e7e669eb6c2b7a5b98560f07865c0fa2520fd
ku22332 <kunalaggarwal@dal.ca>
2017-10-20 11:42:08 -0700

    used nested for statements to read the data

--- 1st.py
+++ 1st.py
@@ -1,3 +1,13 @@
 f = ["Book1.txt", "Book2.txt", "Book3.txt"]
 
-
+def main():
+    big_word = ""
+    for books in f:
+        fin = open(books)
+        for line in fin:
+            line_stripped = line.strip()
+            line_words = line_stripped.split()
+            for word in line_words:
+                if len(word) > len(big_word):
+                    big_word = word
+      
