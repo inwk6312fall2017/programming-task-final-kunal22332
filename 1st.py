1fc90a89cba30c5c8dfee0aaec442dbc2a2d0aaf
ku22332 <kunalaggarwal@dal.ca>
2017-10-20 11:24:51 -0700

    creating the for loop for running the program

--- 1st.py
+++ 1st.py
@@ -7,3 +7,12 @@ l1 = f1.readlines()
 l2 = f1.readlines()
 #l3= f3.readlines()
 
+string = l1[0]
+strsplit = string.split()
+f1 = []
+for k in strsplit:
+         f1.append(len(k))
+         e = max(f1)
+for m in strsplit:
+         if len(m) == e:
+             print("The longest word is", m, "and it is", len(m,"characters long")
